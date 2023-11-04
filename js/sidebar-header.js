06f521795f62a08bb5138546999ca6b4fe05c879
Mario Winter <mariowinter.sg@gmail.com>
2023-11-05 00:32:50 +0100

    Function for sidebar added

--- js/sidebar-header.js
+++ js/sidebar-header.js
@@ -10,3 +10,23 @@ function showSubmenu() {
     isSubMenu = true;
   }
 }
+
+function selectMenuButton(ID) {
+  removeButtonSelection();
+  let button = document.getElementById(ID);
+  button.classList.add('sidebar-button-selected');
+}
+
+function selectPrivacyButton(ID) {
+  removeButtonSelection();
+  let button = document.getElementById(ID);
+  button.classList.add('sidebar-privacy-button-selected');
+}
+
+function removeButtonSelection() {
+  for (let i = 1; i <= 6; i++) {
+    let button = document.getElementById(`sidebar_button${i}`);
+    button.classList.remove('sidebar-button-selected');
+    button.classList.remove('sidebar-privacy-button-selected');
+  }
+}
