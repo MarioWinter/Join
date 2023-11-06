2ecfd8314b828cc3a1800d7ca724b1fe25ca6ac6
GalasMichal <michalgalas1989@gmail.com>
2023-11-06 13:28:06 +0100

    added arrow and return function

--- html.js
+++ html.js
@@ -42,6 +42,20 @@ function renderHtmlLogIn(){
 function renderSingUpHTML(){
     return `
     <div class="sing-up-container">
+        <div onclick="renderLogIn()"
+        class="left-arrow"><svg 
+        
+          xmlns="http://www.w3.org/2000/svg"
+          width="40"
+          height="40"
+          viewBox="0 0 32 32"
+          fill="none"
+        >
+          <path
+            d="M10.4373 14.6667H25.3333C26.0696 14.6667 26.6666 15.2637 26.6666 16.0001C26.6666 16.7364 26.0696 17.3334 25.3333 17.3334H10.4373L16.6466 23.5427C17.1672 24.0634 17.1672 24.9074 16.6466 25.4281C16.126 25.9487 15.2819 25.9487 14.7613 25.4281L6.74746 17.4143C5.96642 16.6332 5.96642 15.3669 6.74747 14.5858L14.7613 6.57206C15.2819 6.05144 16.126 6.05144 16.6466 6.57206C17.1672 7.09268 17.1672 7.93677 16.6466 8.45739L10.4373 14.6667Z"
+            fill="#29ABE2"
+          />
+        </svg></div>
         <h1>Sing up</h1>
         <div class="log-in-underline"></div>
         <div class="sing-up-content">
@@ -170,7 +184,9 @@ function renderSingUpHTML(){
             >
           </div>
         </div>
-        <button style="margin-top: 3.2rem;" class="btn-log-in" disabled>Sing up</button>
+        <button style="margin-top: 3.2rem" class="btn-log-in" disabled>
+          Sing up
+        </button>
       </div>
     `
 }
--- script.js
+++ script.js
@@ -4,13 +4,14 @@
 function renderLogIn(){
     let log_container = document.getElementById('log_container');
     log_container.innerHTML = '';
+    log_container.classList.remove('height-sing-up');
     log_container.innerHTML += renderHtmlLogIn();
 }
 
 function renderSingUp(){
     let log_container = document.getElementById('log_container');
     log_container.innerHTML = '';
-    log_container.style="height: 63.4rem !important";
+    log_container.classList.add('height-sing-up');
     log_container.innerHTML += 
     renderSingUpHTML();
-}
+} 
