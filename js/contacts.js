03ff29a48c4df244fbf29cab14c527b3d1972d55
M-Nafi <nafi-m@gmx.de>
2023-11-14 23:19:56 +0100

    added some visual adjustments

--- js/contacts.js
+++ js/contacts.js
@@ -1,3 +1,12 @@
+// let contactsData = [
+   // {
+      //  name: "Anton Meyer",
+      //  email: "antom@gmail.com",
+      //  phone: "123456789"
+   // },{},
+// ];
+
+
 function showOverlay() {
     document.getElementById('add_new_contact').classList.remove('d-none');
 }
@@ -6,6 +15,20 @@ function cancelOverlay() {
     document.getElementById('add_new_contact').classList.add('d-none');
 }
 
-function cancelEntry() {
-    document.getElementById('') = '';
+function clearEntrys() {
+    document.getElementById('contact_Name').value = '';
+    document.getElementById('contact-Email').value = '';
+    document.getElementById('contact-Phone').value = '';
 }
+
+// function for closing addnewcontact / clicking outside the present window 
+document.addEventListener('click', function(event) {
+    let addContactOverlay = document.getElementById('add_new_contact');
+    let targetElement = event.target;
+    
+    if (targetElement.closest('.overlay-add-contact') === null && !targetElement.classList.contains('primary-contact')) {     
+      addContactOverlay.classList.add('d-none');
+    }
+  });
+
+
