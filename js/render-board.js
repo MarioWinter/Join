eff55bfa37b1c9b6d682b821b4ec709f07f49c69
Mario Winter <mariowinter.sg@gmail.com>
2023-11-25 23:16:29 +0100

    add function for drop task on column
    is not working right now

--- js/render-board.js
+++ js/render-board.js
@@ -1,83 +1,109 @@
+function generateTaskHTML(id, bucket, title, description, duedate, prio, category) {
+    return `
+            <div class="task-card" id="task${id}" onclick="loadTask(${id})" ondragstart="startDragging(${id})"
+            draggable="true">
+            <div class="category-move-card-container">
+                <div class="technical-task">${category}</div>
+                <svg fill="#000000" width="1.6rem" height="1.6rem" viewBox="0 0 16 16"
+                    xmlns="http://www.w3.org/2000/svg" class="mobile-move-card d-none"
+                    onclick="switchToBucket(3, event)">
+                    <path d="M4 14v2l-4-3 4-3v2h12v2H4zm8-12V0l4 3-4 3V4H0V2h12z"
+                        fill-rule="evenodd"></path>
+                </svg>
+            </div>
+            <!-- Title and Description -->
+            <div class="task-title-and-description">
+                <h4 class="task-title" title="${title}">${title}</h4>
+                <div class="task-description">${description}</div>
+            </div>
+            <!-- Subtasks -->
+            
+            <!-- Assignment -->
+            
+        </div>
+    
+    `;
+}
+
+
+
+
 //Template Jason
-addedTasks = [
-    {
-        id: 1,
-        bucket: "in-progress",
-        title: "Kochwelt Page & Recipe Recommender",
-        description: "Build start page with recipe recommendation.",
-        assigned: ["Jad El Nader", "Jonas Lambelet", "Heike Lüdemann"],
-        duedate: "2024-05-10",
-        prio: "Medium",
-        category: "User Story",
-        subtask: [
-            {
-                subdone: true,
-                subtitle: "Implement Recipe Recommendation",
-            },
-            {
-                subdone: false,
-                subtitle: "Start Page Layout",
-            },
-        ],
-    },
-    {
-        id: 2,
-        bucket: "done",
-        title: "CSS Architecture Planning",
-        description: "Define CSS naming conventions and structure.",
-        assigned: ["Alexander Riedel"],
-        duedate: "2023-09-02",
-        prio: "Urgent",
-        category: "Technical Task",
-        subtask: [
-            {
-                subdone: true,
-                subtitle: "Establish CSS Methodology",
-            },
-            {
-                subdone: true,
-                subtitle: "Setup Base Styles",
-            },
-        ],
-    },
-    {
-        id: 3,
-        bucket: "await-feedback",
-        title: "HTML Base Template Creation",
-        description: "Create reusable HTML base templates",
-        assigned: ["Alexander Riedel", "Heike Lüdemann"],
-        duedate: "2024-10-03",
-        prio: "Low",
-        category: "Technical Task",
-        subtask: [],
-    },
-    {
-        id: 4,
-        bucket: "await-feedback",
-        title: "Daily Kochwelt Receipe",
-        description:
-            "Implement daily receipe and portion calculator in JavaScript and HTML",
-        assigned: [
-            "Alexander Riedel",
-            "Jad El Nader",
-            "Jonas Lambelet",
-            "Heike Lüdemann",
-        ],
-        duedate: "2023-09-02",
-        prio: "Urgent",
-        category: "Technical Task",
-        subtask: [
-            {
-                subdone: true,
-                subtitle: "Establish CSS Methodology",
-            },
-            {
-                subdone: true,
-                subtitle: "Setup Base Styles",
-            },
-        ],
-    },
+addedTasks = [{
+    "id": 1,
+    "bucket": "in-progress",
+    "title": "Kochwelt Page & Recipe Recommender",
+    "description": "Build start page with recipe recommendation.",
+    "assigned": ["Jad El Nader", "Jonas Lambelet", "Heike Lüdemann"],
+    "duedate": "2024-05-10",
+    "prio": "Medium",
+    "category": "User Story",
+    "subtask": [
+        {
+            "subdone": true,
+            "subtitle": "Implement Recipe Recommendation"
+        },
+        {
+            "subdone": false,
+            "subtitle": "Start Page Layout"
+        }
+    ]
+},
+{
+    "id": 2,
+    "bucket": "done",
+    "title": "CSS Architecture Planning",
+    "description": "Define CSS naming conventions and structure.",
+    "assigned": ["Alexander Riedel"],
+    "duedate": "2023-09-02",
+    "prio": "Urgent",
+    "category": "Technical Task",
+    "subtask": [
+        {
+            "subdone": true,
+            "subtitle": "Establish CSS Methodology"
+        },
+        {
+            "subdone": true,
+            "subtitle": "Setup Base Styles"
+
+        }
+    ]
+},
+{
+    "id": 3,
+    "bucket": "await-feedback",
+    "title": "HTML Base Template Creation",
+    "description": "Create reusable HTML base templates",
+    "assigned": ["Alexander Riedel", "Heike Lüdemann"],
+    "duedate": "2024-10-03",
+    "prio": "Low",
+    "category": "Technical Task",
+    "subtask": []
+},
+{
+    "id": 4,
+    "bucket": "await-feedback",
+    "title": "Daily Kochwelt Receipe",
+    "description": "Implement daily receipe and portion calculator in JavaScript and HTML",
+    "assigned": ["Alexander Riedel", "Jad El Nader", "Jonas Lambelet", "Heike Lüdemann"],
+    "duedate": "2023-09-02",
+    "prio": "Urgent",
+    "category": "Technical Task",
+    "subtask": [
+        {
+            "subdone": true,
+            "subtitle": "Establish CSS Methodology"
+        },
+        {
+            "subdone": true,
+            "subtitle": "Setup Base Styles"
+
+        }
+    ]
+},
 ];
+
 
 users = [
     {
---
+++ js/task-move-to.js
@@ -0,0 +1,99 @@
+let currentDraggedElement;
+
+function updateBoard(bucket) {
+    let tasks = addedTasks.filter((t) => t["bucket"] == bucket);
+    document.getElementById(bucket).innerHTML = "";
+    for (let index = 0; index < tasks.length; index++) {
+        let task = tasks[index];
+        let id = task['id'];
+        let bucket = task['bucket'];
+        let title = task['title'];
+        let description = task['description'];
+        let duedate = task['duedate'];
+        let prio = task['prio'];
+        let category = task['category'];
+        // let assigneds = loadAssigned(task['assigned']);
+        // let subtasks = loadSubtask(task['subtask']);
+        document.getElementById(bucket).innerHTML +=
+        generateTaskHTML(id, bucket, title, description, duedate, prio, category);
+    } 
+}
+
+
+
+// function updateBoard() {
+//     updateTodo();
+//     updateInProgress();
+//     updateAwaitFeedback();
+//     updateDone();
+// }
+
+
+
+// function updateTodo() {
+//     let todo = addedTasks.filter((t) => t["bucket"] == "todo");
+
+//     document.getElementById("todo").innerHTML = "";
+
+//     for (let index = 0; index < todo.length; index++) {
+//         const element = todo[index];
+//         document.getElementById("todo").innerHTML += generateTaskHTML(element);
+//     } 
+// }
+
+// function updateInProgress() {
+//     let inProgress = addedTasks.filter((t) => t["bucket"] == "in-progress");
+
+//     document.getElementById("in-progress").innerHTML = "";
+
+//     for (let index = 0; index < inProgress.length; index++) {
+//         const element = inProgress[index];
+//         document.getElementById("in-progress").innerHTML += generateTaskHTML(element);
+//     }
+// }
+
+// function updateAwaitFeedback() {
+//     let awaitFeedback = addedTasks.filter((t) => t["bucket"] == "await-feedback");
+
+//     document.getElementById("await-feedback").innerHTML = "";
+
+//     for (let index = 0; index < awaitFeedback.length; index++) {
+//         const element = awaitFeedback[index];
+//         document.getElementById("await-feedback").innerHTML += generateTaskHTML(element);
+//     }
+// }
+
+// function updateDone() {
+//     let done = addedTasks.filter((t) => t["bucket"] == "done");
+
+//     document.getElementById("done").innerHTML = "";
+
+//     for (let index = 0; index < done.length; index++) {
+//         const element = done[index];
+//         document.getElementById("done").innerHTML += generateTaskHTML(element);
+//     }
+// }
+
+
+function startDragging(id) {
+    currentDraggedElement = id;
+}
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function moveTo(bucket) {
+    addedTasks[currentDraggedElement]["bucket"] = bucket;
+    updateBoard(bucket);
+}
+
+function highlight(id) {
+    document.getElementById(id).classList.add("drag-area-highlight");
+}
+
+function removeHighlight(id) {
+    document.getElementById(id).classList.remove("drag-area-highlight");
+}
+
+
