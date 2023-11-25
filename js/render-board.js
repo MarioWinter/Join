354cd85fc6e3a3665e43a93c94a709d2caba6f24
Mario Winter <mariowinter.sg@gmail.com>
2023-11-25 11:34:33 +0100

    add in progess drop section

--- js/render-board.js
+++ js/render-board.js
@@ -1,81 +1,86 @@
-addedTasks = [{
-    "id": 1,
-    "bucket": "in-progress",
-    "title": "Kochwelt Page & Recipe Recommender",
-    "description": "Build start page with recipe recommendation.",
-    "assigned": ["Jad El Nader", "Jonas Lambelet", "Heike Lüdemann"],
-    "duedate": "2024-05-10",
-    "prio": "Medium",
-    "category": "User Story",
-    "subtask": [
-        {
-            "subdone": true,
-            "subtitle": "Implement Recipe Recommendation"
-        },
-        {
-            "subdone": false,
-            "subtitle": "Start Page Layout"
-        }
-    ]
-},
-{
-    "id": 2,
-    "bucket": "done",
-    "title": "CSS Architecture Planning",
-    "description": "Define CSS naming conventions and structure.",
-    "assigned": ["Alexander Riedel"],
-    "duedate": "2023-09-02",
-    "prio": "Urgent",
-    "category": "Technical Task",
-    "subtask": [
-        {
-            "subdone": true,
-            "subtitle": "Establish CSS Methodology"
-        },
-        {
-            "subdone": true,
-            "subtitle": "Setup Base Styles"
-
-        }
-    ]
-},
-{
-    "id": 3,
-    "bucket": "await-feedback",
-    "title": "HTML Base Template Creation",
-    "description": "Create reusable HTML base templates",
-    "assigned": ["Alexander Riedel", "Heike Lüdemann"],
-    "duedate": "2024-10-03",
-    "prio": "Low",
-    "category": "Technical Task",
-    "subtask": []
-},
-{
-    "id": 4,
-    "bucket": "await-feedback",
-    "title": "Daily Kochwelt Receipe",
-    "description": "Implement daily receipe and portion calculator in JavaScript and HTML",
-    "assigned": ["Alexander Riedel", "Jad El Nader", "Jonas Lambelet", "Heike Lüdemann"],
-    "duedate": "2023-09-02",
-    "prio": "Urgent",
-    "category": "Technical Task",
-    "subtask": [
-        {
-            "subdone": true,
-            "subtitle": "Establish CSS Methodology"
-        },
-        {
-            "subdone": true,
-            "subtitle": "Setup Base Styles"
-
-        }
-    ]
-},
+//Template Jason
+addedTasks = [
+    {
+        id: 1,
+        bucket: "in-progress",
+        title: "Kochwelt Page & Recipe Recommender",
+        description: "Build start page with recipe recommendation.",
+        assigned: ["Jad El Nader", "Jonas Lambelet", "Heike Lüdemann"],
+        duedate: "2024-05-10",
+        prio: "Medium",
+        category: "User Story",
+        subtask: [
+            {
+                subdone: true,
+                subtitle: "Implement Recipe Recommendation",
+            },
+            {
+                subdone: false,
+                subtitle: "Start Page Layout",
+            },
+        ],
+    },
+    {
+        id: 2,
+        bucket: "done",
+        title: "CSS Architecture Planning",
+        description: "Define CSS naming conventions and structure.",
+        assigned: ["Alexander Riedel"],
+        duedate: "2023-09-02",
+        prio: "Urgent",
+        category: "Technical Task",
+        subtask: [
+            {
+                subdone: true,
+                subtitle: "Establish CSS Methodology",
+            },
+            {
+                subdone: true,
+                subtitle: "Setup Base Styles",
+            },
+        ],
+    },
+    {
+        id: 3,
+        bucket: "await-feedback",
+        title: "HTML Base Template Creation",
+        description: "Create reusable HTML base templates",
+        assigned: ["Alexander Riedel", "Heike Lüdemann"],
+        duedate: "2024-10-03",
+        prio: "Low",
+        category: "Technical Task",
+        subtask: [],
+    },
+    {
+        id: 4,
+        bucket: "await-feedback",
+        title: "Daily Kochwelt Receipe",
+        description:
+            "Implement daily receipe and portion calculator in JavaScript and HTML",
+        assigned: [
+            "Alexander Riedel",
+            "Jad El Nader",
+            "Jonas Lambelet",
+            "Heike Lüdemann",
+        ],
+        duedate: "2023-09-02",
+        prio: "Urgent",
+        category: "Technical Task",
+        subtask: [
+            {
+                subdone: true,
+                subtitle: "Establish CSS Methodology",
+            },
+            {
+                subdone: true,
+                subtitle: "Setup Base Styles",
+            },
+        ],
+    },
 ];
 
-users = [{
-    
-    "usertoken" : ""
-},
-
+users = [
+    {
+        usertoken: "",
+    },
 ];
