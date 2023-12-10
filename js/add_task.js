23daf3b6583569f347870971619dd57425c26da2
M-Nafi <nafi-m@gmx.de>
2023-12-10 19:51:37 +0100

    added subtask functions

--- js/add_task.js
+++ js/add_task.js
@@ -1,10 +1,6 @@
 let tasks = [];
-let subtaskOptions = 0;
-let text = ['Contact Form', 'Write Legal Imprint'];
-let selectedSubtasks = [];
-let firstSubtaskContainerId = 'subtask_display_first';
-let secondSubtaskContainerId = 'subtask_display_second';
-let isFirstContainerFilled = false;
+let addedSubtasks = [];
+
 
 
 // function for prio area
@@ -68,7 +64,7 @@ function clearAllFields() {
   document.getElementById('add_new_subtask_field').value = '';
   document.getElementById('select_contacts_field').value = '';
   closeSubtaskIcons();
-  document.getElementById('subtask_display_container').innerHTML = '';
+  document.getElementById('subtask_display_container').innerHTML = '';    
 }
 
 async function taskToRemoteStorage(){
@@ -86,28 +82,13 @@ function getDateToday(){
     document.getElementById("date_field").setAttribute('min', today);
 }
 
-// function for switsching the two words 
-function handleSubtaskActions() {
-  let input = document.getElementById('add_new_subtask_field');
-  input.value = text[subtaskOptions % text.length];
-  subtaskOptions++;
-  if (subtaskOptions === text.length) {
-    subtaskOptions;
-  }
-  if (subtaskOptions) {
-    changingSubtaskIcons();    
-  } else {
-    insertSubtaskContainer('subtask_display_first');
-    insertSubtaskContainer('subtask_display_second');
-    closeSubtaskIcons();
-    input.value = '';
-  }    
-}
-
 // function to change the subtask icons for adding 
 function changingSubtaskIcons() {
+  let inputField = document.getElementById('add_new_subtask_field');
   document.getElementById('normal_subtask_icon').classList.add('d-none');
-  document.getElementById('three_subtask_icons').classList.remove('d-none');
+  document.getElementById('three_subtask_icons').classList.remove('d-none');     
+  inputField.focus(); 
+  inputField.select(); 
 }
 
 // function to close the adding icons and back to normal 
@@ -118,38 +99,61 @@ function closeSubtaskIcons() {
   input.value = '';
 }
 
-// function for add the selected word
-function addSubtaskToList() {  
-  let input = document.getElementById('add_new_subtask_field');
-  let subtask = input.value.trim();
+function handleSubtaskActions() {
+  let subtaskInput = document.getElementById('add_new_subtask_field');
+  let subtask = subtaskInput.value.trim(); 
+  changingSubtaskIcons();
   if (subtask !== '') {
-    selectedSubtasks.push(subtask);    
-    input.value = '';
+    addedSubtasks.push(subtask); 
+    displaySubtasks(); 
+    input.value = '';       
   }
-  closeSubtaskIcons(); 
 }
 
-// function for creating containers for selected words
-function insertSubtaskContainer() {
-  let input = document.getElementById('add_new_subtask_field');
-  let subtask = input.value.trim();
+function displaySubtasks() {
+  let subtask = document.getElementById('add_new_subtask_field').value.trim();
   if (subtask !== '') {
-    let containerId = isFirstContainerFilled ? 'subtask_display_second' : 'subtask_display_first';
-    let container = document.getElementById(containerId);
-    if (container) {
-      container.classList.remove('d-none');
-      container.innerHTML += `<div>• ${subtask}</div>`; 
-      isFirstContainerFilled = !isFirstContainerFilled; 
-    } else {
-      console.log("Container not found!");
-    }
+    addedSubtasks.push(subtask);
+    renderAddedSubtasks();
+    document.getElementById('add_new_subtask_field').value = '';
   }
-  closeSubtaskIcons();
 }
 
-// function for creating containers for selected words
-function addSubtaskToContainer(subtask, container) {
-  container.classList.remove('d-none');
-  container.innerHTML += `<div>${subtask}</div>`;
+function renderAddedSubtasks() {
+  let subtaskContainer = document.getElementById('subtask_display_container');
+  subtaskContainer.innerHTML = '';
+  for (let i = 0; i < addedSubtasks.length; i++) {
+    let subtask = addedSubtasks[i];
+    let subtaskDiv = createAddedSubtask(subtask);
+    subtaskContainer.innerHTML += subtaskDiv.outerHTML;
+  }
+  subtaskContainer.classList.remove('d-none');
+  closeSubtaskIcons();  
+}
+
+function createAddedSubtask(subtask) {
+  let subtaskDiv = document.createElement('div');
+  subtaskDiv.classList.add('added-subtask-entry');
+  subtaskDiv.innerHTML = `<span contenteditable="true">&#8226; ${subtask}</span>
+    <div class="subtask-icons">
+      <img id="subtask_icons_1" onclick="editAddedSubtask('${subtask}')" class="subtask-icon" src="./img/pencil-icon.svg">
+      <img id="subtask_icons_2" class="" src="./img/vector-line.svg">
+      <img id="subtask_icons_3" onclick="deleteAddedSubtask('${subtask}')" class="subtask-icon" src="./img/delete-icon.svg">
+      <img id="check_dark_save" class="subtask-icon d-none" src="./img/check-dark.svg">      
+      </div>`;
+  return subtaskDiv;
+}
+
+function deleteAddedSubtask(subtask) {  
+  let index = addedSubtasks.indexOf(subtask);  
+  if (index !== -1) {    
+    addedSubtasks.splice(index, 1);     
+    renderAddedSubtasks();
+  }
+}
+
+function editAddedSubtask() {  
+  document.getElementById(`subtask_icons${i}`).classList.add('d-none');  
+  document.getElementById('check_dark_save').classList.remove('d-none');     
 }
 
