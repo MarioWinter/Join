2222af55a4b63c3d834df43d5823cec2c56160d4
Mario Winter <mariowinter.sg@gmail.com>
2024-01-07 01:29:53 +0100

    fixed witdh from board mobile header search input field

--- js/board.js
+++ js/board.js
@@ -2,282 +2,302 @@ let addedTasks = [];
 let storageTasks = [];
 let filteredTasks = [];
 
-
 async function initBoard() {
-    await loadAddedTasks();
-    await loadUsers();
-    loadBoard();
-    loadCurrentUser();
-    loadUserBadge();
-    
+	await loadAddedTasks();
+	await loadUsers();
+	loadBoard();
+	loadCurrentUser();
+	loadUserBadge();
 }
-
 
 /**
  * hidden function to clear Added Tasks Remote Storage from any information
  */
-async function clearAddedTasksRemoteSTRG(){
-    addedTasks = [];
-    await setItem("addedTasks", JSON.stringify(addedTasks));
+async function clearAddedTasksRemoteSTRG() {
+	addedTasks = [];
+	await setItem("addedTasks", JSON.stringify(addedTasks));
 }
-
 
 async function loadAddedTasks() {
-    try {
-        addedTasks = JSON.parse(await getItem("addedTasks"));
-    } catch (e) {
-      console.error("Loading Added Tasks error:", e);
-    }
+	try {
+		addedTasks = JSON.parse(await getItem("addedTasks"));
+	} catch (e) {
+		console.error("Loading Added Tasks error:", e);
+	}
 }
-
 
 function loadBoard() {
-    for (let i = 0; i < buckets.length; i++) {
-        let bucket = buckets[i];
-        updateBoard(bucket);
-        loadNoTasksLabel(bucket);
-    }
+	for (let i = 0; i < buckets.length; i++) {
+		let bucket = buckets[i];
+		updateBoard(bucket);
+		loadNoTasksLabel(bucket);
+	}
 }
-
 
 function updateBoard(currentBucket) {
-    let tasks = getTasksPerBucket(currentBucket);
-    for (let index = 0; index < tasks.length; index++) {
-        let [id, bucket, title, description, prio, category, subtasks, assigneds, duedate, rawDuedate] = getTaskVariables(tasks, index);
-        loadCard(id, bucket, title, description, prio, category, subtasks, assigneds);
-    }
+	let tasks = getTasksPerBucket(currentBucket);
+	for (let index = 0; index < tasks.length; index++) {
+		let [
+			id,
+			bucket,
+			title,
+			description,
+			prio,
+			category,
+			subtasks,
+			assigneds,
+			duedate,
+			rawDuedate,
+		] = getTaskVariables(tasks, index);
+		loadCard(
+			id,
+			bucket,
+			title,
+			description,
+			prio,
+			category,
+			subtasks,
+			assigneds
+		);
+	}
 }
-
 
 function getTasksPerBucket(currentBucket) {
-    let tasks = [];
-    if (filteredTasks.length == 0) {
-        tasks = addedTasks.filter((t) => t["bucket"] == currentBucket);
-    } else {
-        tasks = filteredTasks.filter((t) => t["bucket"] == currentBucket);
-    }
-    document.getElementById(currentBucket).innerHTML = "";
-    return tasks;
+	let tasks = [];
+	if (filteredTasks.length == 0) {
+		tasks = addedTasks.filter((t) => t["bucket"] == currentBucket);
+	} else {
+		tasks = filteredTasks.filter((t) => t["bucket"] == currentBucket);
+	}
+	document.getElementById(currentBucket).innerHTML = "";
+	return tasks;
 }
-
 
 function getTaskVariables(tasks, index) {
-        let task = tasks[index];
-        let id = task['id'];
-        let bucket = task['bucket'];
-        let title = task['title'];
-        let description = task['description'];
-        let prio = task['prio'];
-        let category = task['category'];
-        let subtasks = task['subtask'];
-        let assigneds = task['assigned'];
-        let duedate = formatDueDate(task['duedate']);
-        let rawDuedate = task['duedate'];
-        return [id, bucket, title, description, prio, category, subtasks, assigneds, duedate, rawDuedate];
+	let task = tasks[index];
+	let id = task["id"];
+	let bucket = task["bucket"];
+	let title = task["title"];
+	let description = task["description"];
+	let prio = task["prio"];
+	let category = task["category"];
+	let subtasks = task["subtask"];
+	let assigneds = task["assigned"];
+	let duedate = formatDueDate(task["duedate"]);
+	let rawDuedate = task["duedate"];
+	return [
+		id,
+		bucket,
+		title,
+		description,
+		prio,
+		category,
+		subtasks,
+		assigneds,
+		duedate,
+		rawDuedate,
+	];
 }
 
-
-function loadCard(id, bucket, title, description, prio, category, subtasks, assigneds) {
-    let categoryColor = loadCategoryColor(category);
-    document.getElementById(bucket).innerHTML +=
-    generateCardHTML(id, title, description, category, categoryColor);
-    loadSubtaskprogress(subtasks, id);
-    loadAssigneds(assigneds, id);
-    loadCardPrioIcon(prio, id);
+function loadCard(
+	id,
+	bucket,
+	title,
+	description,
+	prio,
+	category,
+	subtasks,
+	assigneds
+) {
+	let categoryColor = loadCategoryColor(category);
+	document.getElementById(bucket).innerHTML += generateCardHTML(
+		id,
+		title,
+		description,
+		category,
+		categoryColor
+	);
+	loadSubtaskprogress(subtasks, id);
+	loadAssigneds(assigneds, id);
+	loadCardPrioIcon(prio, id);
 }
-
 
 function loadNoTasksLabel(bucket) {
-    let taskColumn = document.getElementById(bucket);
-    if (taskColumn.innerHTML === '') {
-        let formatBucket = formatNoTaskLabelString(bucket);
-        taskColumn.innerHTML = generateNoTaskHTML(formatBucket);
-    }
+	let taskColumn = document.getElementById(bucket);
+	if (taskColumn.innerHTML === "") {
+		let formatBucket = formatNoTaskLabelString(bucket);
+		taskColumn.innerHTML = generateNoTaskHTML(formatBucket);
+	}
 }
-
 
 function loadSubtaskprogress(subtasks, id) {
-    let allSubtask = subtasks.length;
-    let done = loadSubtaskAreDone(subtasks);
-    if (allSubtask > 0) {
-    document.getElementById(`subtasks_container_${id}`).innerHTML = 
-    generateSubtaskProgressHTML(allSubtask, done);
-    }
+	let allSubtask = subtasks.length;
+	let done = loadSubtaskAreDone(subtasks);
+	if (allSubtask > 0) {
+		document.getElementById(`subtasks_container_${id}`).innerHTML =
+			generateSubtaskProgressHTML(allSubtask, done);
+	}
 }
-
 
 function loadAssigneds(assigneds, id) {
-    for (let i = 0; i < assigneds.length; i++) {
-        let [badgeColor, userBadge, assignedLimit, addLimit] = getVariableForAssignedsUserBadge(assigneds, i);
-        if(i <= addLimit) {
-            addAssignedBadge(userBadge, badgeColor, id);
-        } else if(i == assignedLimit && assigneds.length > 6) {
-            addLimitAssignedBadge(id, assigneds, assignedLimit);
-        }
-    };
+	for (let i = 0; i < assigneds.length; i++) {
+		let [badgeColor, userBadge, assignedLimit, addLimit] =
+			getVariableForAssignedsUserBadge(assigneds, i);
+		if (i <= addLimit) {
+			addAssignedBadge(userBadge, badgeColor, id);
+		} else if (i == assignedLimit && assigneds.length > 6) {
+			addLimitAssignedBadge(id, assigneds, assignedLimit);
+		}
+	}
 }
-
 
 function getVariableForAssignedsUserBadge(assigneds, i) {
-    let badgeColor = getUserColor(assigneds, i);
-    let assignedUserName = assigneds[i];
-    let userBadge = generateUserBadge(assignedUserName);
-    let assignedLimit = assigneds.length - 1;
-    let addLimit = 5;
-    return [badgeColor, userBadge, assignedLimit, addLimit];
+	let badgeColor = getUserColor(assigneds, i);
+	let assignedUserName = assigneds[i];
+	let userBadge = generateUserBadge(assignedUserName);
+	let assignedLimit = assigneds.length - 1;
+	let addLimit = 5;
+	return [badgeColor, userBadge, assignedLimit, addLimit];
 }
 
-
 function addAssignedBadge(userBadge, badgeColor, id) {
-    document.getElementById(`task_assignment_container_${id}`).innerHTML +=
-    generateAssignedBadgeHTML(userBadge, badgeColor);
+	document.getElementById(`task_assignment_container_${id}`).innerHTML +=
+		generateAssignedBadgeHTML(userBadge, badgeColor);
 }
 
 function addLimitAssignedBadge(id, assigneds) {
-    let limit = assigneds.length - 6;
-    document.getElementById(`task_assignment_container_${id}`).innerHTML += `<div class="assigned-limit">+${limit}</div>`;
+	let limit = assigneds.length - 6;
+	document.getElementById(
+		`task_assignment_container_${id}`
+	).innerHTML += `<div class="assigned-limit">+${limit}</div>`;
 }
-
 
 function loadCardPrioIcon(prio, id) {
-    let taskPrioIcon = document.getElementById(`task_prio_img_${id}`);
-    if (prio === "Urgent") {
-        taskPrioIcon.innerHTML = generateUrgentPrioIcon();
-    } else if (prio === "Medium") {
-        taskPrioIcon.innerHTML = generateMediumPrioIcon();
-    } else if (prio === "Low") {
-        taskPrioIcon.innerHTML = generateLowPrioIcon();
-    }
+	let taskPrioIcon = document.getElementById(`task_prio_img_${id}`);
+	if (prio === "Urgent") {
+		taskPrioIcon.innerHTML = generateUrgentPrioIcon();
+	} else if (prio === "Medium") {
+		taskPrioIcon.innerHTML = generateMediumPrioIcon();
+	} else if (prio === "Low") {
+		taskPrioIcon.innerHTML = generateLowPrioIcon();
+	}
 }
-
 
 function loadSubtaskAreDone(subtasks) {
-    let done = 0;
-    for (let i = 0; i < subtasks.length; i++) {
-        let subtask = subtasks[i];
-        if (subtask.subdone){
-            done++
-        }
-    }
-    return done;
+	let done = 0;
+	for (let i = 0; i < subtasks.length; i++) {
+		let subtask = subtasks[i];
+		if (subtask.subdone) {
+			done++;
+		}
+	}
+	return done;
 }
-
 
 function loadCategoryColor(category) {
-    if(category === 'Technical Task') {
-        return '#1fd7c1';
-    } else if (category === 'User Story') {
-        return '#0038FF';
-    }
+	if (category === "Technical Task") {
+		return "#1fd7c1";
+	} else if (category === "User Story") {
+		return "#0038FF";
+	}
 }
-
 
 function show(id) {
-    document.getElementById(id).classList.remove('d-none');
+	document.getElementById(id).classList.remove("d-none");
 }
-
 
 function hide(id) {
-    document.getElementById(id).classList.add('d-none');
+	document.getElementById(id).classList.add("d-none");
 }
-
 
 function showFrame(id) {
-    addOverlayBg(id);
-    show(id);
+	addOverlayBg(id);
+	show(id);
 }
-
 
 async function deleteTask(TaskID) {
-    let updatedAddedTasks = addedTasks.filter(task => task.id !== TaskID);
-    addedTasks = updatedAddedTasks;
-    hideTaskOpen('task_open_overlay_frame');
-    loadBoard();
+	let updatedAddedTasks = addedTasks.filter((task) => task.id !== TaskID);
+	addedTasks = updatedAddedTasks;
+	hideTaskOpen("task_open_overlay_frame");
+	loadBoard();
 }
-
 
 /**
  * returns the completed subtasks as a percentage
- * 
+ *
  * @param {int} allSubtask - All stubtasks of a task
  * @param {*} done  - all completed subtasks of a task
- * @returns 
+ * @returns
  */
 function generatePercentInWidth(allSubtask, done) {
-    let percentInWidth = done / allSubtask * 100;
-    return percentInWidth
+	let percentInWidth = (done / allSubtask) * 100;
+	return percentInWidth;
 }
-
 
 function formatNoTaskLabelString(str) {
-    str = str.charAt(0).toUpperCase() + str.slice(1)
-    let formattedStr = str.replace('-', ' ');
-    return formattedStr;
+	str = str.charAt(0).toUpperCase() + str.slice(1);
+	let formattedStr = str.replace("-", " ");
+	return formattedStr;
 }
-
 
 /**
  * Converts the date to the correct format
  * @param {string} dueDate - Contains the date from the DueDate specification
- * @returns 
+ * @returns
  */
 function formatDueDate(dueDate) {
-    if(dueDate.includes('-')) {
-        let dateParts = dueDate.split('-');
-        let duedate = dateParts[2] + '/' + dateParts[1]+ '/' + dateParts[0];
-        return duedate;
-    }
-    return dueDate;
+	if (dueDate.includes("-")) {
+		let dateParts = dueDate.split("-");
+		let duedate = dateParts[2] + "/" + dateParts[1] + "/" + dateParts[0];
+		return duedate;
+	}
+	return dueDate;
 }
-
 
 async function clearRemoteStorage() {
-    users = [];
-    await setItem("users", JSON.stringify(users));
+	users = [];
+	await setItem("users", JSON.stringify(users));
 }
 
-
-
 function searchTask() {
-    let searchTerm = find_task.value;
-    clearBoard();
-    filteredTasks = addedTasks.filter((t) => t['title'].toLowerCase().includes(searchTerm.toLowerCase()));
-    loadBoard();
-    errorNoteSearchTask('no_task_found');
+	let searchTerm = find_task.value;
+	clearBoard();
+	filteredTasks = addedTasks.filter((t) =>
+		t["title"].toLowerCase().includes(searchTerm.toLowerCase())
+	);
+	loadBoard();
+	errorNoteSearchTask("no_task_found");
 }
 
 function searchTaskMobile() {
-    let searchTerm = find_task_mobile.value;
-    clearBoard();
-    filteredTasks = addedTasks.filter((t) => t['title'].toLowerCase().includes(searchTerm.toLowerCase()));
-    loadBoard();
-    errorNoteSearchTask('no_task_found_mobile');
+	let searchTerm = find_task_mobile.value;
+	clearBoard();
+	filteredTasks = addedTasks.filter((t) =>
+		t["title"].toLowerCase().includes(searchTerm.toLowerCase())
+	);
+	loadBoard();
+	errorNoteSearchTask("no_task_found_mobile");
 }
-
 
 function errorNoteSearchTask(searchID) {
-    if(filteredTasks.length == 0) {
-        document.getElementById(searchID).style.display = "block"
-    }
+	if (filteredTasks.length == 0) {
+		document.getElementById(searchID).style.display = "block";
+	}
 }
 
-
-function  clearBoard() {
-    for (let i = 0; i < buckets.length; i++) {
-        let bucket = buckets[i];
-        document.getElementById(bucket).innerHTML = "";
-    }
+function clearBoard() {
+	for (let i = 0; i < buckets.length; i++) {
+		let bucket = buckets[i];
+		document.getElementById(bucket).innerHTML = "";
+	}
 }
 
 function closeFilter() {
-    let searchTerm = find_task.value;
-    let searchTermMobile = find_task_mobile.value;
-    searchTerm = searchTerm.toLowerCase();
-    searchTermMobile = searchTermMobile.toLowerCase();
-    if(searchTerm.length == 0 || searchTermMobile == 0) {
-        document.getElementById('no_task_found').style.display = "none";
-        document.getElementById('no_task_found_mobile').style.display = "none"
-        filteredTasks = [];
-        loadBoard();
-    }
+	let searchTerm = find_task.value;
+	searchTerm = searchTerm.toLowerCase();
+	if (searchTerm.length == 0) {
+		document.getElementById("no_task_found").style.display = "none";
+		filteredTasks = [];
+		loadBoard();
+	}
 }
--- js/task-move-to.js
+++ js/task-move-to.js
@@ -1,43 +1,72 @@
 let currentDraggedElement;
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
+	document
+		.getElementById("find_task")
+		.addEventListener("keyup", function (event) {
+			if (event.key === "Enter") {
+				searchTask();
+			}
+		});
 
-    document.getElementById("find_task").addEventListener("keyup", function(event) {
-        if (event.key === "Enter") {
-            searchTask();
-        }
-    });
+	let dropZoneIds = buckets;
+
+	dropZoneIds.forEach(function (dropZoneId) {
+		let dropZone = document.getElementById(dropZoneId);
+		let longPressTimeout;
+
+		if (dropZone) {
+			dropZone.addEventListener("touchstart", function (event) {
+				let targetCard = event.target.closest(".task-card");
+
+				if (targetCard) {
+					longPressTimeout = setTimeout(function () {
+						let cardIdString = targetCard.id.replace("task", "");
+						// Rufe die startDragging-Funktion auf
+						let cardId = parseInt(cardIdString, 10);
+						startDragging(cardId);
+						// moveTo(dropZoneId);
+						// removeHighlight(dropZoneId);
+						console.log("Long Press erkannt!");
+					}, 500); // Hier ist die Zeitverzögerung für den Long Press in Millisekunden (500ms in diesem Beispiel)
+				}
+			});
+
+			dropZone.addEventListener("touchend", function () {
+				clearTimeout(longPressTimeout);
+			});
+		}
+	});
 });
 
 /**
  * Saves the current task ID
- * @param {int} id - ID from the drag elements 
+ * @param {int} id - ID from the drag elements
  */
 function startDragging(id) {
-    currentDraggedElement = id;
+	currentDraggedElement = id;
 }
 
-
 function allowDrop(ev) {
-    ev.preventDefault();
+	ev.preventDefault();
 }
 
 /**
  * The position of the column is changed in the array and the board is reloaded
- * @param {String} bucket - HTML Id from the drop zone 
+ * @param {String} bucket - HTML Id from the drop zone
  */
 async function moveTo(bucket) {
-    addedTasks[currentDraggedElement]["bucket"] = bucket;
-    loadBoard();
-    await setItem("addedTasks", JSON.stringify(addedTasks));
+	addedTasks[currentDraggedElement]["bucket"] = bucket;
+	loadBoard();
+	await setItem("addedTasks", JSON.stringify(addedTasks));
 }
 
 /**
  * The target dropzone is highlighted when draging
- * @param {int} id - ID from the drag elements 
+ * @param {int} id - ID from the drag elements
  */
 function highlight(id) {
-    document.getElementById(id).classList.add("drag-area-highlight");
+	document.getElementById(id).classList.add("drag-area-highlight");
 }
 
 /**
@@ -45,7 +74,5 @@ function highlight(id) {
  * @param {int} id - ID from the drag elements
  */
 function removeHighlight(id) {
-    document.getElementById(id).classList.remove("drag-area-highlight");
+	document.getElementById(id).classList.remove("drag-area-highlight");
 }
-
-
